3b63f1ba05cfaabad04062abea90d91f00d4f7fb
Cibbi <12004047+Cibbi@users.noreply.github.com>
2022-07-13 10:10:05 +0000

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p $(dirname $3)
-dotnet run --project "/unity-package-exporter/UnityPackageExporter/UnityPackageExporter.csproj" -i "$1" --asset-root "$2" -o "$3"
+dotnet run --project "/unity-package-exporter/UnityPackageExporter/UnityPackageExporter.csproj" "$1" "$3" --asset-root "$2"
 echo "::set-output name=artifactsPath::$3"
